ca45c32ed4ee96520a854843a4f465379b9df146
Nathan Robinson <nrobinson2000@me.com>
2017-01-21 22:17:43 -0500

    Initial commit at 22:17 of 21-01-2017

--- firmware/particle-BNO055.cpp
+++ firmware/particle-BNO055.cpp
@@ -9,13 +9,19 @@
   These sensors use I2C to communicate, 2 pins are required to interface.
 
   Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
+  please support Adafruit and open-source hardware by purchasing products
   from Adafruit!
 
   Written by KTOWN for Adafruit Industries.
 
   MIT license, all text above must be included in any redistribution
- ***************************************************************************/
+
+  Ported to Particle devices by Nathan Robinson
+  https://github.com/nrobinson2000
+  On 2017-1-21
+
+****************************************************************************/
+
 #include "Particle.h"
 #include "OneWire.h"
 #include <math.h>
